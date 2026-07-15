e75287772769043194e46a78cc222d9fe7cf3acb
thongkeobeer-ship-it <thongkeobeer@gmail.com>
2026-07-15 08:43:39 +0700

    Create  supabase.js

---
+++ supabase.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = "https://pwaizgmbodfyjacigsrf.supabase.co";
+const SUPABASE_KEY = "sb_publishable_BaqRrigHl9AW-k1WhftakQ_rBo9ju7M";
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
