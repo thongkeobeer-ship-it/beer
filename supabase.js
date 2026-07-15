7d72ac76688a309bc4eb604009066d6df6a71bb4
thongkeobeer-ship-it <thongkeobeer@gmail.com>
2026-07-15 07:19:30 +0700

    Create  supabase.js

---
+++ supabase.js
@@ -0,0 +1,9 @@
+ const SUPABASE_URL = "https://pwaizgmbodfyjacigsrf.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_BaqRrigHl9AW-k1WhftakQ_rBo9ju7M";
+
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
